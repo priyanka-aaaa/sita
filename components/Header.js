62108fd02bfba52558683cc3d925790796839fe1
priyanka <priyanka.calinfo500@gmail.com>
2023-08-04 23:44:00 +0530

    zxz

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from "next/image"
 import Link from "next/link";
-// import Logo from "../public/logo.jpg";
+
 export const Header = () => {
     return (
         <nav className="navbar navbar-expand-sm bg-light navbar-light">
@@ -9,8 +9,7 @@ export const Header = () => {
                 <ul className="navbar-nav">
                     <li className="nav-item">
                         <img src="/images/logo.jpg" className="nav-logo" alt="logo" />
-                        {/* <Image src={Logo} width="100" height="100"/> */}
-                        {/* <img src="image/logo.jpg" className="nav-logo" alt="logo"/> */}
+                       
                     </li>
                     <li className="nav-item">
                         <Link legacyBehavior href='/' className="nav-link" >
@@ -18,33 +17,33 @@ export const Header = () => {
                         </Link>
                     </li>
                     <li className="nav-item">
-                        <Link legacyBehavior href='/' className="nav-link" >
+                        <Link legacyBehavior href='/event' className="nav-link" >
                             <a className="nav-link nav-event" href="#">EVENTS</a>
                         </Link>
                     </li>
                     <li className="nav-item">
-                        <Link legacyBehavior href='/' className="nav-link" >
+                        <Link legacyBehavior href='/result' className="nav-link" >
                             <a className="nav-link" href="#">RESULT</a>
                         </Link>
                     </li>
                     <li className="nav-item">
-                        <Link legacyBehavior href='/' className="nav-link" >
+                        <Link legacyBehavior href='/service' className="nav-link" >
                             <a className="nav-link" href="#">SERVICES</a>
                         </Link>
                     </li>
                     <li className="nav-item">
-                        <Link legacyBehavior href='/' className="nav-link" >
+                        <Link legacyBehavior href='/rental' className="nav-link" >
                             <a className="nav-link" href="#">RENTAL
                                 EQUIPMENT</a>
                         </Link>
                     </li>
                     <li className="nav-item">
-                        <Link legacyBehavior href='/' className="nav-link" >
+                        <Link legacyBehavior href='/gallery' className="nav-link" >
                             <a className="nav-link" href="#">GALLERY</a>
                         </Link>
                     </li>
                     <li className="nav-item">
-                        <Link legacyBehavior href='/' className="nav-link" >
+                        <Link legacyBehavior href='/contact' className="nav-link" >
                             <a className="nav-link" href="#">CONTACT</a>
                         </Link>
                     </li>

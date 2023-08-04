09ad12c902f0f6ab894a401f9db1f8b7b65e4aee
priyanka <priyanka.calinfo500@gmail.com>
2023-08-05 00:29:02 +0530

    zxxcxzcx

--- pages/service.js
+++ pages/service.js
@@ -71,82 +71,23 @@ export default function Home() {
             <button type="button" class="btn btn-info latest-event">Latest Events</button>
           </div>
         </div>
-        <section class="event-sch">
-          <div class="row">
-            <div class="col-md-4">
-              <div class="event-schedule">
-                EVENT SCHEDULE
-              </div>
-            </div>
-            <div class="col-md-4">
-              <p class="event-schedule-para">Lorem lipsem is simply dummy text of the printing and typesetting industry</p>
-            </div>
-            <div class="col-md-4">
-              <div class="schedule-view-more">
-              <Link legacyBehavior href='/' className="nav-link" >
-                            <a className="viewmore" >View More</a>
-                        </Link>
-
+    </div>
+       <section class="rental-sport">
+        <div class="row">
+        <div class="latest-event">
+            <button type="button" class="btn btn-info latest-event">Supports Equipment</button>
+          </div>
+          <h2 className='rental-sport-heading'>
+          RENTAL SPORTS EQUIPMENT THAT YOU NEED  
+          </h2>
+          <div className="row btn-enquire-submit">
+                  <button type="submit" className="btn purchase-submit">
+                    <span className="quick_reg">ENQUIRE NOW</span>
+                  </button>
                 </div>
-            </div>
-          </div>
-        </section>
-        <div class=" event-type">
-          <p><a>Active events</a></p>
-          <p><a>Upcoming events</a></p>
-          <p><a>Live events</a></p>
-          <p><a>Past events</a></p>
-
-          
         </div>
-        <section class="event-list">
-          <div class="row">
-            <div class="col-md-2">
-              <p class="date"> 04<br />June,2023</p>
-            </div>
-            <div class="col-md-8">
-              <img src="/images/event.png" class="event-img" />
-              <div class="championship">
-                <h3 class="chamion-heading">
-                  Cycle Race Championship
-                </h3>
-                <p class="chamion-para">New York Jun 4,2023, 8:00-Jun 9,2023 17:00 </p>
-              </div>
-            </div>
-            <div class="col-md-2">
-              <p class="price">$55 </p>
-            </div>
-          </div>
-        </section>
-        <section class="what-say">
-          <div class="row">
-            <div class="col-md-6">
-              <div class="TESTIMOVIALS-details">
-                <div class="test-content">
-
-              
-              <div class="testmineral">
-                TESTIMOVIALS
-              </div>
-              <h3 class="what-they">
-                WHAT THEY SAY
-              </h3>
-              <p class="testmineral-para">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
-              </p>
-              <div class="quotes">
-         
-              <img src="/images/quote.png" className="quote" alt="logo" />
-
-              </div>
-            </div>
-            </div>
-            </div>
-            <div class="col-md-6">
-              <img src="/images/what-say.png" alt="what-say" />
-            </div>
-          </div>
-        </section>
+       </section>
+       <div class="container-fluid home-detail">
         <div class="sport-brand">
           <img src="/images/sport-brand1.jpg" className="sport-brand-img" alt="logo" />
           <img src="/images/sport-brand2.jpg" className="sport-brand-img" alt="logo" />
